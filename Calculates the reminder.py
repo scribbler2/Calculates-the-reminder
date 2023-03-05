fe63bbb606ab0df717dc9835e8d8086d2ff9e8cd
Dragan Mirilo <draganmirilo86@gmail.com>
2023-03-05 19:59:54 +0100

    Add files via upload

---
+++ Calculates the reminder.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+remainder = num1 % num2
+
+print("The remainder when", num1, "is divided by", num2, "is", remainder)
+
+
+# In[ ]:
+
+
+
+
